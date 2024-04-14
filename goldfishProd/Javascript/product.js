f0b71d47aac75cd0f65bd11817f76d2014844bdd
jpark116 <jamespark116@gmail.com>
2024-04-14 16:14:40 -0400

    Signed-off-by: jpark116 <jamespark116@gmail.com>

--- goldfishProd/Javascript/product.js
+++ goldfishProd/Javascript/product.js
@@ -1,6 +1,18 @@
 
-document.getElementById(button1).addEventListener("click", Hiding)
-function hiding(){
-    document.getElementById
+function hide(){
+    var x = document.getElementById("lorem")
+    if (x.style.display === "block"){
+        x.style.display = "none";
+    } else{
+        x.style.display="block";
+    }
+    
+
 }    
 
+function cartTotal(p1, p2, p3, p4){
+    return p1 + p2 + p3 + p4;
+}
+
+let result = cartTotal(1, 2, 3, 4);
+document.getElementById("total").innerHTML = result;
